e62564635662fa4534578e7ca992751e8f9dca09
Caitlin Elliott <ceelliott90@gmail.com>
2022-01-31 10:12:34 -0500

    added home.ejs view

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(4000, () => {
     console.log('serving on port 4000')
 });
 
+
 app.get('/', (req, res) => {
-    res.send('hello from yelpcamp');
+    res.render('home');
 })
