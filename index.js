22f4843bc44d587e3f3e1ce11b7ddfbd35878210
Caitlin Elliott <ceelliott90@gmail.com>
2022-01-31 10:15:17 -0500

    created campground.js schema

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Sets our view location to the home.ejs file
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Spins up the server
 app.listen(4000, () => {
     console.log('serving on port 4000')
 });
